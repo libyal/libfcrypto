0a884739f01f351b38e58d84541f562aee11bc63
Joachim Metz <joachim.metz@gmail.com>
2017-09-01 12:45:57 +0200

    Worked on library

---
+++ libfcrypto/libfcrypto_types.h
@@ -0,0 +1,47 @@
+/*
+ * The internal type definitions
+ *
+ * Copyright (C) 2017, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined( _LIBFCRYPTO_INTERNAL_TYPES_H )
+#define _LIBFCRYPTO_INTERNAL_TYPES_H
+
+#include <common.h>
+#include <types.h>
+
+/* Define HAVE_LOCAL_LIBFCRYPTO for local use of libfcrypto
+ * The definitions in <libfcrypto/types.h> are copied here
+ * for local use of libfcrypto
+ */
+#if defined( HAVE_LOCAL_LIBFCRYPTO )
+
+/* The following type definitions hide internal data structures
+ */
+#if defined( HAVE_DEBUG_OUTPUT ) && !defined( WINAPI )
+
+
+#else
+
+
+#endif /* defined( HAVE_DEBUG_OUTPUT ) && !defined( WINAPI ) */
+
+#endif /* defined( HAVE_LOCAL_LIBFCRYPTO ) */
+
+#endif /* !defined( _LIBFCRYPTO_INTERNAL_TYPES_H ) */
+
---
+++ tests/test_library.ps1
@@ -0,0 +1,116 @@
+# Tests C library functions and types.
+#
+# Version: 20170827
+
+$ExitSuccess = 0
+$ExitFailure = 1
+$ExitIgnore = 77
+
+$LibraryTests = "error support"
+$LibraryTestsWithInput = ""
+
+$TestToolDirectory = "..\msvscpp\Release"
+
+Function RunTest
+{
+	param( [string]$TestType )
+
+	$TestDescription = "Testing: ${TestName}"
+	$TestExecutable = "${TestToolDirectory}\fcrypto_test_${TestName}.exe"
+
+	$Output = Invoke-Expression ${TestExecutable}
+	$Result = ${LastExitCode}
+
+	If (${Result} -ne ${ExitSuccess})
+	{
+		Write-Host ${Output} -foreground Red
+	}
+	Write-Host "${TestDescription} " -nonewline
+
+	If (${Result} -ne ${ExitSuccess})
+	{
+		Write-Host " (FAIL)"
+	}
+	Else
+	{
+		Write-Host " (PASS)"
+	}
+	Return ${Result}
+}
+
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\msvscpp\VSDebug"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2010\Release"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2010\VSDebug"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2012\Release"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2012\VSDebug"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2013\Release"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2013\VSDebug"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2015\Release"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2015\VSDebug"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2017\Release"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	$TestToolDirectory = "..\vs2017\VSDebug"
+}
+If (-Not (Test-Path ${TestToolDirectory}))
+{
+	Write-Host "Missing test tool directory." -foreground Red
+
+	Exit ${ExitFailure}
+}
+
+$Result = ${ExitIgnore}
+
+Foreach (${TestName} in ${LibraryTests} -split " ")
+{
+	$Result = RunTest ${TestName}
+
+	If (${Result} -ne ${ExitSuccess})
+	{
+		Break
+	}
+}
+
+Foreach (${TestName} in ${LibraryTestsWithInput} -split " ")
+{
+	# TODO: add RunTestWithInput
+	$Result = RunTest ${TestName}
+
+	If (${Result} -ne ${ExitSuccess})
+	{
+		Break
+	}
+}
+
+Exit ${Result}
+
