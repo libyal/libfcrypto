c35d375f5fa6850a1eb978aee7a91d257e039328
Joachim Metz <joachim.metz@gmail.com>
2025-11-27 10:31:41 +0100

    Applied updates

--- include/libfcrypto/error.h
+++ include/libfcrypto/error.h
@@ -1,7 +1,7 @@
 /*
  * The error code definitions for libfcrypto
  *
- * Copyright (C) 2017-2024, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2017-2025, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- tests/fcrypto_test_des3_context.c
+++ tests/fcrypto_test_des3_context.c
@@ -1,7 +1,7 @@
 /*
  * Library des3_context type test program
  *
- * Copyright (C) 2017-2024, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2017-2025, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
