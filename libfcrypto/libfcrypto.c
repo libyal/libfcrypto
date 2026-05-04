575ca4718debbd649727dbd85b1b421bf7c70e9c
Joachim Metz <joachim.metz@gmail.com>
2026-05-04 15:20:46 +0200

    Applied updates

--- libfcrypto/libfcrypto.c
+++ libfcrypto/libfcrypto.c
@@ -1,7 +1,7 @@
 /*
  * Library to support encryption formats
  *
- * Copyright (C) 2017-2025, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2017-2026, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- tests/fcrypto_test_serpent_context.c
+++ tests/fcrypto_test_serpent_context.c
@@ -1,7 +1,7 @@
 /*
  * Library serpent_context type test program
  *
- * Copyright (C) 2017-2025, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2017-2026, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
